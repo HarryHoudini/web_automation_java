392676939ee66d045aecdd21fba9d5cfdd4d13de
Aleksandr Elmekeev <donamok@gmail.com>
2019-02-10 00:32:23 +0300

    Enabled screenshots even for passed tests.

--- src/test/java/com/inadequate/web/automation/common/SportmasterTest.java
+++ src/test/java/com/inadequate/web/automation/common/SportmasterTest.java
@@ -1,12 +1,15 @@
 package com.inadequate.web.automation.common;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.testng.ScreenShooter;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+@Listeners({ ScreenShooter.class})
 public abstract class SportmasterTest {
     private Properties envProperties;
 
@@ -22,9 +25,10 @@ public abstract class SportmasterTest {
         Configuration.browser = "chrome";
         Configuration.headless = true;
         Configuration.reportsFolder = "reports";
-
         // Tests often fails with default timeout on my machine due to performance
         Configuration.timeout = 10000;
+
+        ScreenShooter.captureSuccessfulTests = true;
     }
 
     private Properties readEnvironmetProperties() {
